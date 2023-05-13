230cbec417dd83f85a39baee9b442346c312fd5c
HugoSanche <hbmclaren@gmail.com>
2023-05-13 11:30:57 -0600

    Checked and unchecked exceptions. Working with finally

---
+++ src/Checked_UnChecked_Exceptions.java
@@ -0,0 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Checked_UnChecked_Exceptions {
+    //Checked Exceptions.- Exceptions are IOExceptions,FileNotFoundExceptions, ArithmeticException, ArrayStoreException
+
+    //Unchecked Exceptions are.- Run time exceptions.- like NullPointerExceptions, IndexOutOfBoundsExceptions,ArithmeticException
+
+    public static void main(String[] args) {
+    //this generates Unchecked exceptions.- null pointer exceptions
+    String string=null;
+    printLength(string);
+
+    //this generates Checked exceptions.-
+
+    }
+    public static void printLength(String string){
+        int myString=string.length();
+        System.out.println(myString);
+
+        readFile("myfile.txt");
+    }
+
+    public static void readFile(String fileName){
+        try {
+            FileReader fileReader = new FileReader(fileName); //this is checked Exceptions if you not write try catch you don't make compile
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+    }
+}
